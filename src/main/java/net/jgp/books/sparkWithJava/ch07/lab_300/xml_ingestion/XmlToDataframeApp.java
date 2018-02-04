3cfeec4c2f59fa164aa366a77520ac503ab730be
Jean Georges Perrin <jgp@lumeris.com>
2018-02-04 12:19:41 -0600

    Test on case sensitivity.

--- src/main/java/net/jgp/books/sparkWithJava/ch07/lab_300/xml_ingestion/XmlToDataframeApp.java
+++ src/main/java/net/jgp/books/sparkWithJava/ch07/lab_300/xml_ingestion/XmlToDataframeApp.java
@@ -35,7 +35,7 @@ public class XmlToDataframeApp {
         .getOrCreate();
 
     // Reads a CSV file with header, called books.csv, stores it in a dataframe
-    Dataset<Row> df = spark.read().format("xml")
+    Dataset<Row> df = spark.read().format("XML")
         .option("rowTag", "row")
         .load("data/nasa-patents.xml");
 
